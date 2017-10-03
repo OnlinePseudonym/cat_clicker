1a1478cc24b1b19cdc231925fe1465ff02e985a7
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-10-03 13:48:09 -0700

    v0.02 add second cat

--- js/app.js
+++ js/app.js
@@ -1,38 +1,40 @@
 (function() {
+  const cats = ['Mittens', 'Ser Pounce'];
+  const elem = document.getElementById('cat-pictures');
+  const counter = document.getElementById('click-counter');
+  let clickCounter = 0;
 
-    const elem = document.getElementById('cat-picture');
-    const counter = document.getElementById('click-counter');
-    let clickCounter = 0;
-
-    elem.addEventListener('click', function() {
-        clickCounter += 1;
-        counter.innerHTML = clickCounter;
-    }, false);
-
-    fetch('https://api.unsplash.com/search/photos?page=1&query=Cat', {
-        headers: {
-            Authorization: 'Client-ID c2a8dc48e99f388612aaceab726995bfb362915795f0a63527a37c9dfef7c1dd'
-        }
-    }).then(response => response.json())
-    .then(addImage)
-    .catch(e => requestError(e, 'image'));
-
-    function addImage(data) {
-        let htmlContent = '';
-        let random;
-        const image = data.results[random = (Math.floor(Math.random() * (data.results.length)))];
-        console.log(image);
-        if (image) {
-            htmlContent = `<figure>
-                <img src="${image.urls.small}" alt="Cat">
-                <figcaption>by ${image.user.name}</figcaption>
-            </figure>`;
-            console.log(htmlContent)
-        } else {
-            htmlContent = 'Unfortunately, we couldn\'t find a cat.'
-        }
-        console.log(htmlContent)
-
-        elem.insertAdjacentHTML('afterbegin', htmlContent);
+  fetch('https://api.unsplash.com/search/photos?page=1&query=Cat', {
+    headers: {
+      Authorization: 'Client-ID c2a8dc48e99f388612aaceab726995bfb362915795f0a63527a37c9dfef7c1dd'
     }
+  }).then(response => response.json())
+  .then(addImages)
+  .catch(e => requestError(e, 'image'));
+
+  function addImages(data) {
+    for( let i = 0; i < cats.length; i++) {
+      let htmlContent = '';
+      const image = data.results[Math.floor(Math.random() * (data.results.length))];
+      const cat = cats[i].toLowerCase().replace(' ', '-');
+      if (image) {
+        htmlContent = `<h2>${cats[i]}</h2><figure>
+        <img id="${cat}" src="${image.urls.small}" alt="Cat">
+        <figcaption>by ${image.user.name}</figcaption>
+        <div id="${cat}-counter">0</div>
+        </figure>`;
+      } else {
+        htmlContent = 'Unfortunately, we couldn\'t find a cat.'
+      }
+      elem.insertAdjacentHTML('afterbegin', htmlContent);
+      setListener(cat);
+    }
+  }
+  function setListener(target) {
+    document.getElementById(target).addEventListener('click', function() {
+      let content = document.getElementById(`${target}-counter`).innerHTML;
+      console.log(content);
+      document.getElementById(`${target}-counter`).innerHTML = (parseInt(content)+1).toString();
+    });
+  }
 })();
